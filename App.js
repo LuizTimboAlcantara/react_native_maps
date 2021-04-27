d7dad69b6175ff11834fb685034b2bf2e9197adb
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-04-26 22:57:59 -0300

    Events.

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
@@ -13,29 +14,69 @@ export default class App extends Component {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       },
+      texto: '',
     };
 
-    this.moverCidade = this.moverCidade.bind(this);
+    //#region  Binds
+
+    // this.moverCidade = this.moverCidade.bind(this);
+    // this.mudouMapa = this.mudouMapa.bind(this);
+    this.clicou = this.clicou.bind(this);
+
+    //#endregion
   }
 
-  moverCidade(lat, long) {
-    let state = this.state;
-    let region = {
-      latitude: lat,
-      longitude: long,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    };
-    state.region = region;
-    this.setState(state);
+  //#region Função MoverCidade
+
+  // moverCidade(lat, long) {
+  //   let state = this.state;
+  //   let region = {
+  //     latitude: lat,
+  //     longitude: long,
+  //     latitudeDelta: 0.0922,
+  //     longitudeDelta: 0.0421,
+  //   };
+  //   state.region = region;
+  //   this.setState(state);
+  // }
+
+  //#endregion
+
+  //#region  Função Mudou Mapa
+
+  // mudouMapa(region) {
+  //   let state = this.state;
+  //   state.texto = region.latitude;
+  //   state.region = {
+  //     latitude: region.latitude,
+  //     longitude: region.longitude,
+  //     latitudeDelta: 0.0922,
+  //     longitudeDelta: 0.0421,
+  //   };
+  //   this.setState(state);
+  // }
+
+  //#endregion
+
+  //#region Fução Clicou
+
+  clicou(event) {
+    alert(
+      'Latitude:' +
+        event.nativeEvent.coordinate.latitude +
+        '\nLongitude:' +
+        event.nativeEvent.coordinate.longitude,
+    );
   }
+
+  //#endregion
 
   render() {
-    const {region} = this.state;
+    const {region, texto} = this.state;
 
     return (
       <View style={styled.container}>
-        <View style={{flexDirection: 'row'}}>
+        {/* <View style={{flexDirection: 'row'}}>
           <Button
             title="Fortaleza"
             onPress={() => {
@@ -48,13 +89,28 @@ export default class App extends Component {
               this.moverCidade(-15.8080374, -47.8750231);
             }}
           />
-        </View>
+        </View> */}
 
         <Text>
           {region.latitude} | {region.longitude}
         </Text>
+        <Text>Latitude Atual</Text>
+        <Text>{texto}</Text>
 
-        <MapView style={styled.maps} region={region} />
+        <MapView
+          // Chamado quando carrega totalmente o mapa
+          // onMapReady={() => {
+          //   alert('Mapa carregado');
+          // }}
+
+          // Evento disparado quando vc arrasta o mapa e solta;
+          // onRegionChangeComplete={this.mudouMapa}
+
+          //  Evento para fazer algo quando se clica no mapa;
+          onPress={this.clicou}
+          style={styled.maps}
+          region={region}
+        />
       </View>
     );
   }
