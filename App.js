9d31b6d6ab1905265cc15d1be3ed90ec0616a76f
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-04-26 22:36:19 -0300

    Mudando de localização.

--- App.js
+++ App.js
@@ -1,23 +1,63 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
 import MapView from 'react-native-maps';
 
-export default function app() {
-  return (
-    <View style={styled.container}>
-      <Text>Mapas</Text>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: -3.71839,
+        longitude: -38.5434,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
 
-      <MapView
-        style={{width: '100%', height: '100%'}}
-        initialRegion={{
-          latitude: -3.71839,
-          longitude: -38.5434,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    </View>
-  );
+    this.moverCidade = this.moverCidade.bind(this);
+  }
+
+  moverCidade(lat, long) {
+    let state = this.state;
+    let region = {
+      latitude: lat,
+      longitude: long,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    };
+    state.region = region;
+    this.setState(state);
+  }
+
+  render() {
+    const {region} = this.state;
+
+    return (
+      <View style={styled.container}>
+        <View style={{flexDirection: 'row'}}>
+          <Button
+            title="Fortaleza"
+            onPress={() => {
+              this.moverCidade(-3.71839, -38.5434);
+            }}
+          />
+          <Button
+            title="Brasilia"
+            onPress={() => {
+              this.moverCidade(-15.8080374, -47.8750231);
+            }}
+          />
+        </View>
+
+        <Text>
+          {region.latitude} | {region.longitude}
+        </Text>
+
+        <MapView style={styled.maps} region={region} />
+      </View>
+    );
+  }
 }
 
 const styled = StyleSheet.create({
@@ -25,5 +65,10 @@ const styled = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  maps: {
+    width: '100%',
+    height: '90%',
   },
 });
